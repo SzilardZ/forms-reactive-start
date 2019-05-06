1ddace584ef99da6ce6fb5e77395bdad412b66a3
SzilardZ <szilard.zr@gmail.com>
2019-05-06 12:26:30 +0200

    typo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email], this.forbinddenEmails),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([]),
@@ -65,7 +65,7 @@ export class AppComponent implements OnInit {
     return null;
   }
 
-  forbinddenEmails(control: FormControl): Promise<any> | Observable<any> {
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
     const promise = new Promise<any>((resolve, reject) => {
       setTimeout(() => {
         if (control.value === 'test@test.com') {
